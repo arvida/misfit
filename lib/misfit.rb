6d6d9d18b569afbdfd4c262039afc9696218b420
arvida <arvid@winstondesign.se>
2010-04-22 14:31:17 +0200

    Added suport for non svn projects

--- lib/misfit.rb
+++ lib/misfit.rb
@@ -16,7 +16,7 @@ require 'thor'
 require 'yaml'
  
 class Misfit < Thor
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
   
   desc "misfit version", "Prints version information"
   def version
@@ -24,10 +24,16 @@ class Misfit < Thor
   end
   
   desc "misfit import GIT_REPOSITORY_URL DESTINATION", "Import GIT_REPOSITORY_URL to DESTINATION"
+  method_options :branch => :string
   def import(repository_url, destination)
     if not File.exists?(destination)
       puts "Importing #{repository_url} to #{destination}"
-      puts import_repository(repository_url, destination) ? `svn status #{destination}` : "Unable to clone #{repository_url}"
+      if import_repository(repository_url, destination, options[:branch])
+        puts 'Imported repository'
+        puts `svn status #{destination}` if svn_project?
+      else
+        puts "Unable to clone #{repository_url}"
+      end
     else
       puts "#{destination} already exists"
     end
@@ -38,13 +44,22 @@ class Misfit < Thor
     dest_config = config_for_path(destination)
     if dest_config and dest_config['repository_url']
       puts "Importing updates from #{dest_config['repository_url']} to #{destination}"
-      puts update_repository(dest_config['repository_url'], destination) ? `svn status #{destination}` : "Unable to update #{destination} from #{dest_config['repository_url']}"
+      if update_repository(dest_config['repository_url'], destination, dest_config['branch'])
+        puts 'Updated repository'
+        puts `svn status #{destination}` if svn_project?
+      else
+        "Unable to update #{destination} from #{dest_config['repository_url']}"
+      end
     else
       puts "Unable to open #{destination}/.misfit.yaml"
     end
   end
   
 protected
+
+  def svn_project?
+    @svn_project ||= File.exists?('.svn')
+  end
 
   def config_for_path(path)
     settings_file = File.join(path, '.misfit.yaml')
@@ -53,21 +68,28 @@ protected
     end
   end
   
-  def import_repository(repository_url, destination)
+  def import_repository(repository_url, destination, branch = nil)
     `git clone #{repository_url} #{destination}`
+    if branch
+      `cd #{destination};git checkout -b #{branch} origin/#{branch}`
+    end
     if $?.to_i == 0
       `rm -Rf #{destination}/.git`
       File.open(File.join(destination, '.misfit.yaml'), 'w') do |f|  
-        f.puts({ 'repository_url' => repository_url }.to_yaml)
+        f.puts({ 'repository_url' => repository_url, 'branch' => branch }.to_yaml)
       end
-      `svn add #{destination}` and return true
+      `svn add #{destination}` if svn_project?
+      return true
     end
     false
   end
   
-  def update_repository(repository_url, destination)
+  def update_repository(repository_url, destination, branch = nil)
     tmp_dest = File.dirname(destination)+'_tmp'
     `git clone #{repository_url} #{tmp_dest}`
+    if branch
+      `cd #{tmp_dest};git checkout -b #{branch} origin/#{branch}`
+    end
     if $?.to_i == 0
       sync_directories(destination, tmp_dest)
       `rm -Rf #{tmp_dest}` and return true
@@ -81,7 +103,7 @@ protected
     old_directory_paths.each do |path|
       new_path = path.gsub(/^#{old_directory}\//, new_directory+'/')
       unless new_directory_paths.include?(new_path)
-        puts `svn remove #{path}`
+        puts `svn remove #{path}` if svn_project?
         `rm -rf #{path}`
       end
     end
@@ -99,7 +121,7 @@ protected
         `cp -f #{path} #{new_path}`
       end
       if do_add
-        `svn add #{new_path}`
+        `svn add #{new_path}` if svn_project?
       end
     end
   end
